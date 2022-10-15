27d651a68bdf335bd77f68fe35c8e14a22026ad6
Timo Katila <timo@tkatila.com>
2022-10-15 13:54:38 +0300

    Navigaatio uusittu. sekä artikkeleita muotoiltu

--- src/components/NewsArticles.js
+++ src/components/NewsArticles.js
@@ -1,52 +1,35 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getNews } from "../actions/NewsActions";
 import Article from "./Article";
+import { Grid } from "@mui/material"
 
 const NewsArticles = (props) => {
 
   const dispatch = useDispatch();
 
   //USE EFFECT ajetaan ennen renderöintiä. Se  hakee dispatchilla tiedot. Esim pelaajilla dispatch(getPlayers())
-
   useEffect(() => {
     dispatch(getNews())
   }, []);
 
-
   const appState = useSelector((state) => state);
   console.log(appState)
-  let articles = appState.news.news.newsArticles.map((article) => {
 
+  let articles = appState.news.news.newsArticles.map((article) => {
     return (
-      < tr key={article.id} >
-        <td> {(article.date)}</td>
-        <td> {article.header} </td>
-        <td> {article.content}</td>
-      </tr >
+
+      <Grid item xs={12} sm={6} md={4}>
+        <Article key={article.id} header={article.header} date={article.date} content={article.content} />
+      </Grid >
     )
   })
 
   return (
-    <div>
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Päivämäärä</th>
-            <th>Otsikko</th>
-            <th>Sisältö</th>
 
-          </tr>
-        </thead>
-        <tbody>
-          {articles}
-        </tbody>
-      </table>
-
-
-      <Article />
-    </div>
-
+    <Grid container spacing={2} alignItems="center" justify="center">
+      {articles}
+    </Grid>
   )
 }
 
