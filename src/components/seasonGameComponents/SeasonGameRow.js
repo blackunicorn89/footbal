5c7a118047269b40969fcbb24cde1d94d2d729bc
Timo Katila <timo@tkatila.com>
2022-11-16 17:46:43 +0200

    Bug fixes

--- src/components/seasonGameComponents/SeasonGameRow.js
+++ src/components/seasonGameComponents/SeasonGameRow.js
@@ -27,19 +27,11 @@ const SeasonGameRow = (props) => {
   let date = new Date(props.date)
   let localDate = date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear();
 
-
-  //ALLA OLEVA SIIRRETTY SHARED/COMPONENTS/DELETECONFIRMATION.JS:SSÄÄÄN VOI EHKÄ POISTAA???
-  // const removeSingleSeasonGame = (id) => {
-  //   dispatch(removeSeasonGame(appState.login.token, id))
-
-  // };
-
-
   const players = props.players
-  const listOfPlayers = players.map((player) =>  <li>{player}</li>);
+  const listOfPlayers = players.map((player) => <li>{player}</li>);
 
   const goalMakers = props.goalmakers
-  const listOfGoalMakers = goalMakers.map((goalMaker) =>  <li>{goalMaker}</li>);
+  const listOfGoalMakers = goalMakers.map((goalMaker) => <li>{goalMaker}</li>);
 
   if (appState.login.admin) {
 
@@ -74,15 +66,15 @@ const SeasonGameRow = (props) => {
           <Typography variant="body1" component="pre" sx={{ marginTop: 3 }} >
             <ul>
               {listOfPlayers}
-            </ul>        
+            </ul>
           </Typography>
           <Typography variant="h5" component="pre" sx={{ marginTop: 3 }} >
             Maalintekijät:
           </Typography>
           <Typography variant="body1" component="pre" sx={{ marginTop: 3 }} >
             <ul>
-            {listOfGoalMakers}  
-            </ul>      
+              {listOfGoalMakers}
+            </ul>
           </Typography>
           <Typography variant="body1" component="pre" sx={{ marginTop: 3 }} >
             Lisätietoa pelistä: {props.description}
@@ -92,7 +84,7 @@ const SeasonGameRow = (props) => {
     )
   } else {
     return (
-      <Card sx={{ minWidth: 275, maxWidth: "md", margin: "auto", backgroundColor: "#A60201", color: "#73c6b6" }}>
+      <Card sx={{ minWidth: 275, maxWidth: "md", margin: "auto" }}>
         <CardHeader
           title={props.season}
         />
